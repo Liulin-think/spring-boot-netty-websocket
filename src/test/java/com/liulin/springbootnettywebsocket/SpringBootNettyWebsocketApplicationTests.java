c7266780e0d63fb14cfaeb651ee30c3d6779c2a9
liulin_think <570389384@qq.com>
2020-07-06 17:50:46 +0800

    Initial commit

---
+++ src/test/java/com/liulin/springbootnettywebsocket/SpringBootNettyWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liulin.springbootnettywebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootNettyWebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
